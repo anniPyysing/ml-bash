7ce5edd26f4a23c3ce396c0960d67cf53da0a909
anniPyysing <39370168+anniPyysing@users.noreply.github.com>
2019-01-14 13:42:10 +0200

    Update ml-bash-file.bash

--- ml-bash-file.bash
+++ ml-bash-file.bash
@@ -1,7 +1,7 @@
 ## Script that downloads
 ## some Jupyter notebooks 
 
-cd /home/jovyan/work
+cd /home/jovyan
 
 # git reflog requires a name and email if user is not in passwd
 # even if you're only cloning
@@ -11,6 +11,6 @@ export GIT_COMMITTER_EMAIL=anon@localhost
 git clone https://github.com/anniPyysing/ml.git
 
 rmdir work
-#rm get-started.bash
+rm ml-bash-file.bash
 
 pip install numpy --upgrade
